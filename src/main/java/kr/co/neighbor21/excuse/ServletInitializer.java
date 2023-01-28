dcbffde877b3d84a6fd0c9917f4770682b017164
kiljunhee <hihih123@neighbor21.co.kr>
2023-01-28 23:21:51 +0900

    메인 333

--- src/main/java/kr/co/neighbor21/excuse/ServletInitializer.java
+++ src/main/java/kr/co/neighbor21/excuse/ServletInitializer.java
@@ -10,6 +10,6 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(WebApplication.class);
+		return application.sources(WebApplication.class);//메인메인33
 	}
 }
