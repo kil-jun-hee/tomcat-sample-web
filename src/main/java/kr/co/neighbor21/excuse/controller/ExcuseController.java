5226f56077114dbae068165706beeed775e3b42d
kiljunhee <hihih123@neighbor21.co.kr>
2023-01-28 22:47:22 +0900

    메인 브랜치 커밋

--- src/main/java/kr/co/neighbor21/excuse/controller/ExcuseController.java
+++ src/main/java/kr/co/neighbor21/excuse/controller/ExcuseController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ExcuseController {
 	
-	@GetMapping("/home")
+	@GetMapping("/home")//main 브랜치 테스트
 	public String home(Model model, HttpSession session) throws Exception {
 
 		
